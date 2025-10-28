ca6db4f5624f02e86a6b518ddce6def598c241ba
26nsmall <26nsmall@gannacademy.org>
2025-10-27 22:25:36 -0400

    c

--- src/below_zero.py
+++ src/below_zero.py
@@ -3,7 +3,10 @@
 import pandas as pd
 
 def below_zero():
-    pass
+    df = pd.read_csv("src\kumpula-weather-2017.csv")
+    mask = df["Air temperature (degC)"] < 0
+    print(mask)
+    return len(df[mask])
 
 def main():
     result = below_zero()
